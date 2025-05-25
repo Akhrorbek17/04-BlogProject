15a3c5ef9a3b10cdffe0fe888432eb2344d18018
Azamov Akhrorbek <azamovahrorbek2277@gmail.com>
2025-05-25 20:48:33 +0500

    Herokuga mosladim

--- config/settings.py
+++ config/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'blog',
     'accounts',
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,14 +119,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT= os.path.join(BASE_DIR , 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL= '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR  , 'media')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR , 'staticfiles')
 ),
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 LOGIN_REDIRECT_URL ='blog:home'
 LOGOUT_REDIRECT_URL ='blog:home'
 # Default primary key field type
